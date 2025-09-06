757e26915495df4a2abb9a713c73add4d438986c
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-09-06 09:28:41 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+
+function App() {
+const [clickNumber, setClickNumber] = useState<number>(() => {
+	const stored = (localStorage.getItem("clickNumber"))
+	return stored ? JSON.parse(stored) as number : 0;
+});
+	//huehue
+
+	useEffect(() => {
+		localStorage.setItem("clickNumber", JSON.stringify(clickNumber));
+	},[clickNumber])
+
+  	return (
+	<>
+		<div className="flex gap-5">
+			<button
+			className="bg-amber-500 cursor-pointer"
+			onClick={() => {
+				setClickNumber(clickNumber + 1)
+			}}
+			>Increase</button>
+			<button
+				className=""
+				onClick={() => {
+					setClickNumber(clickNumber - 1)
+				}}
+			>Decrease</button>
+
+			<button
+				onClick={() => {
+					setClickNumber(0);
+				}}
+			>Reset</button>
+
+			<h1
+			className=""
+			>
+				Click number = {clickNumber}
+			</h1>
+		</div>
+    </>
+  )
+}
+
+export default App
